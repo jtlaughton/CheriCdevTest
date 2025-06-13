ca1473bb4c0c7ed04fd54984e762f569166eee3a
Tyllis Xu <LivelyCarpet87@gmail.com>
2025-06-13 08:19:56 -0500

    Update client.c

--- client/client.c
+++ client/client.c
@@ -132,33 +132,33 @@ int main(void) {
 
   sleep(1);
   printf("Attempting to send as a different user.\n");
+   uint32_t victim = cdev_disc_req.found_receivers[1];
+  if (victim == my_id) {
+    victim = cdev_disc_req.found_receivers[0];
+  }
   tx_cdev_req_t tx_cdev_req_bad_id;
   tx_cdev_req_bad_id.cap_req = cap_request;
-  tx_cdev_req_bad_id.my_id = 1;
+  tx_cdev_req_bad_id.my_id = victim;
   strncpy(cdev_buffer->transmit_buffer, "Hello World!", 12);
   tx_cdev_req_bad_id.length = 13;
   tx_cdev_req_bad_id.receiver_id = my_id;
   if (ioctl(cdev_cheri_fd, CDEV_TX, &tx_cdev_req_bad_id) < 0) {
         printf("Driver rejected forged request.\n");
+  } else {
+    printf("Driver failed to reject forged request from %d as %d.\n", my_id, victim);
   }
 
   printf("Attempting to send without capability token.\n");
-  uint32_t victim = cdev_disc_req.found_receivers[1];
-  if (victim == my_id) {
-    victim = cdev_disc_req.found_receivers[0];
-  }
   cap_req_t bad_cap_request;
   tx_cdev_req_t tx_cdev_req_bad_cap;
   bad_cap_request.user_cap = malloc(4096); // placeholder
   tx_cdev_req_bad_cap.cap_req = bad_cap_request;
-  tx_cdev_req_bad_cap.my_id = victim;
+  tx_cdev_req_bad_cap.my_id = my_id;
   strncpy(cdev_buffer->transmit_buffer, "Hello World!", 12);
   tx_cdev_req_bad_cap.length = 13;
   tx_cdev_req_bad_cap.receiver_id = my_id;
   if (ioctl(cdev_cheri_fd, CDEV_TX, &tx_cdev_req_bad_cap) < 0) {
         printf("Driver rejected invalid request.\n");
-  } else {
-    printf("Driver failed to reject invalid request %d as %d.\n", my_id, victim);
   }
 
 
